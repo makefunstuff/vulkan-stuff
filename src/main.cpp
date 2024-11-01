9f5d72bb8a6a0ac2590f93e46659fc1e6d8f666f
iurii <hello@jurip.xyz>
2024-11-02 01:31:28 +0200

    fixed

--- src/main.cpp
+++ src/main.cpp
@@ -906,7 +906,7 @@ void CreateGraphicsPipeline()
     rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
     rasterizer.lineWidth = 1.0f;
     rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
-    rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
     rasterizer.depthBiasEnable = VK_FALSE;
     rasterizer.depthBiasConstantFactor = 0.0f; // Optional
     rasterizer.depthBiasClamp = 0.0f; // Optional
@@ -1434,6 +1434,7 @@ int main() {
     CreateImageViews();
     CreateRenderPass();
     CreateDescriptorSetLayout();
+
     CreateGraphicsPipeline();
     CreateFrameBuffers();
     CreateCommandPool();
@@ -1441,6 +1442,7 @@ int main() {
     CreateIndexBuffer();
     CreateUniformBuffers();
     CreateDescriptorPool();
+    CreateDescriptorSets();
     CreateCommandBuffers();
     CreateSyncObjects();
 
